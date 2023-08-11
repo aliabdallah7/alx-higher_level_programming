26235ac8a0b4e5ce00d85ba0825d448d1b4b1d83
aliabdallah7 <ali.abdallah43792@gmail.com>
2023-08-11 11:01:41 -0700

    Update

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,30 +1,28 @@
 #!/usr/bin/python3
-import sys
-from calculator_1 import add, sub, mul, div
-
-def main():
-    if len(sys.argv) != 4:
-        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-        sys.exit(1)
-    
-    a = int(sys.argv[1])
-    operator = sys.argv[2]
-    b = int(sys.argv[3])
-    
-    if operator == '+':
-        result = add(a, b)
-    elif operator == '-':
-        result = sub(a, b)
-    elif operator == '*':
-        result = mul(a, b)
-    elif operator == '/':
-        result = div(a, b)
-    else:
-        print("Unknown operator. Available operators: +, -, * and /")
-        sys.exit(1)
-    
-    print(f"{a} {operator} {b} = {result}")
-    sys.exit(0)
 
 if __name__ == "__main__":
-    main()
+    import sys
+    import calculator_1 as calc
+
+    len = len(sys.argv) - 1
+
+    if len != 3:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        sys.exit(1)
+
+    a, op, b = sys.argv[1:]
+    a = int(a)
+    b = int(b)
+
+    if op not in "+-*/":
+        print("Unknown operator. Available operators: +, -, * and /")
+        sys.exit(1)
+
+    if op == '+':
+        print("{} {} {} = {}".format(a, op, b, calc.add(a, b)))
+    if op == '-':
+        print("{} {} {} = {}".format(a, op, b, calc.sub(a, b)))
+    if op == '*':
+        print("{} {} {} = {}".format(a, op, b, calc.mul(a, b)))
+    if op == '/':
+        print("{} {} {} = {}".format(a, op, b, calc.div(a, b)))
